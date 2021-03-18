ff5af57708189c40f05dea1117af96236ff41d0a
Jakeas <JakeASierra@gmail.com>
2021-03-18 17:56:56 -0400

    working on the stretch remove from cart feature

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -4,7 +4,7 @@ import { CartContext } from '../contexts/CartContext'
 // Components
 import Item from './ShoppingCartItem';
 
-const ShoppingCart = props => {
+const ShoppingCart = () => {
 
 	const { cart } = useContext(CartContext)
 
@@ -13,13 +13,13 @@ const ShoppingCart = props => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
-
+	
 	return (
 		<div className="shopping-cart">
 			{cart.map(item => (
 				<Item key={item.id} {...item} />
 			))}
-
+			<div></div>
 			<div className="shopping-cart__checkout">
 				<p>Total: ${getCartTotal()}</p>
 				<button>Checkout</button>
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CartContext } from '../contexts/CartContext'
 
-const Item = props => {
+const Item = (props) => {
+
+	const { cart } = useContext(CartContext)
+		
+	const handleClickRemove = () =>{
+		//what goes in here?
+	}
+
+	const removeItem = (remove) => {
+		// let cartCopy = [...cart]
+		 cart.filter((cartItem) => cartItem.id !== remove)
+		// return setCart(cartCopy)
+	}
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
 
 
+	
+
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button onClick={handleClickRemove}>Remove from cart</button>
 			</div>
 		</div>
 	);
